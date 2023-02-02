d83cc236f3398b543bd36a8b166abd725d6bbafa
oliveiramiro2 <ramiromares95@gmail.com>
2023-02-02 17:08:10 -0300

    added layout of download page

--- src/Pages/private/Downloads/index.tsx
+++ src/Pages/private/Downloads/index.tsx
@@ -1,12 +1,50 @@
-import { View, Text } from 'react-native';
+import { View, ScrollView, Text, Image, TouchableOpacity } from 'react-native';
 import React from 'react';
+import { EvilIcons } from '@expo/vector-icons';
 
 import styles from './styles';
 
+const download = require('../../../assets/imgs/download.png');
+
 const Download: React.FC = () => (
-    <View style={[styles.contain]}>
-        <Text style={[styles.text]}>1</Text>
-    </View>
+    <ScrollView style={[styles.contain]}>
+        <View style={[styles.containContent]}>
+            <View style={[styles.containSmart]}>
+                <EvilIcons
+                    name="gear"
+                    color="#a9a9a9"
+                    size={23}
+                    style={[styles.ml5]}
+                />
+                <Text style={[styles.textSmart, styles.ml5]}>
+                    Smart download
+                </Text>
+            </View>
+
+            <View style={[styles.containDescribe]}>
+                <Text style={[styles.TextTitle]}>
+                    Conheça o Downloads para você
+                </Text>
+
+                <Text style={[styles.textSmart, styles.textDescribe]}>
+                    Vamos baixar uma seleção personalizada de filmes e séries
+                    para você sempre ter um título para assistir no celular.
+                </Text>
+
+                <Image source={download} style={[styles.imgDownload]} />
+
+                <TouchableOpacity style={[styles.buttonBlue]}>
+                    <Text style={[styles.textButton1]}>Configurar</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={[styles.buttonGray]}>
+                    <Text style={[styles.textButton2]}>
+                        Encontre mais títulos para baixar
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    </ScrollView>
 );
 
 export default Download;
--- src/Pages/private/Downloads/styles.tsx
+++ src/Pages/private/Downloads/styles.tsx
@@ -7,9 +7,82 @@ const styles = StyleSheet.create({
         backgroundColor: metrics.colors.color1,
     },
 
-    text: {
+    containContent: {
+        flex: 1,
+        justifyContent: 'space-between',
+        backgroundColor: metrics.colors.color1,
+        margin: 5,
+        paddingTop: metrics.dimensions.height * 0.12,
+        paddingBottom: metrics.dimensions.height * 0.01,
+    },
+
+    containSmart: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+
+    ml5: {
+        marginLeft: 8,
+    },
+
+    containDescribe: {
+        marginTop: 40,
+        paddingHorizontal: 15,
+    },
+
+    textSmart: {
         fontFamily: metrics.fonts.defaultRegular,
+        fontSize: 15,
         color: metrics.colors.color4,
+    },
+
+    TextTitle: {
+        fontFamily: metrics.fonts.defaultRegular,
+        fontSize: 15,
+        color: metrics.colors.color3,
+        marginBottom: 10,
+    },
+
+    textDescribe: {
+        fontSize: 12,
+    },
+
+    imgDownload: {
+        width: metrics.dimensions.width - 50,
+        height: metrics.dimensions.height * 0.38,
+        resizeMode: 'cover',
+        marginTop: 50,
+    },
+
+    buttonBlue: {
+        width: metrics.dimensions.width - 20,
+        backgroundColor: '#2361e7',
+        height: 45,
+        alignSelf: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 5,
+        marginTop: 20,
+    },
+
+    textButton1: {
+        color: metrics.colors.color3,
+        fontWeight: 'bold',
+    },
+
+    buttonGray: {
+        backgroundColor: metrics.colors.color5,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        marginTop: 40,
+        alignSelf: 'center',
+        borderRadius: 5,
+    },
+
+    textButton2: {
+        color: metrics.colors.color3,
+        fontWeight: 'bold',
+        fontSize: 18,
     },
 });
 
