54b13699ae1567c40e9cef6f0bf60085609079c7
oliveiramiro2 <ramiromares95@gmail.com>
2023-02-02 18:04:42 -0300

    added quick laugh pages

--- src/Pages/private/QuickLaugh/index.tsx
+++ src/Pages/private/QuickLaugh/index.tsx
@@ -1,12 +1,95 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import {
+    View,
+    Text,
+    FlatList,
+    ImageBackground,
+    Image,
+    TouchableOpacity,
+} from 'react-native';
+import React, { useState } from 'react';
+import { AntDesign, Feather, FontAwesome, Octicons } from '@expo/vector-icons';
 
+import { videoQL } from '../../../services/Utils';
+import { metrics } from '../../../services/metrics';
 import styles from './styles';
 
-const QuickLaugh: React.FC = () => (
-    <View style={[styles.contain]}>
-        <Text style={[styles.text]}>1</Text>
+interface IPropsVideoQl {
+    mute: boolean;
+    setMute: Function;
+}
+
+const VideoQL: React.FC<IPropsVideoQl> = ({ mute, setMute }) => (
+    <View style={[styles.containQL]}>
+        <ImageBackground
+            source={{
+                uri: `https://api.lorem.space/image/movie?w=${
+                    Math.round(Math.random() * 1960) + 40
+                }&h=${Math.round(Math.random() * 1960) + 40}`,
+            }}
+            resizeMode="cover"
+            style={[styles.containShort]}
+        >
+            <View style={[styles.containAge]}>
+                <Text style={[styles.text]}>L</Text>
+            </View>
+            <View style={[styles.containIconsRight]}>
+                <Image
+                    source={{ uri: 'https://api.lorem.space/image/face' }}
+                    style={[styles.containIconAvatar]}
+                />
+                <View style={[styles.containIconRight]}>
+                    <AntDesign name="smile-circle" size={30} color="#fff" />
+                    <Text style={[styles.text, styles.textIconsRight]}>
+                        LOL
+                    </Text>
+                </View>
+                <View style={[styles.containIconRight]}>
+                    <AntDesign name="plus" size={30} color="#fff" />
+                    <Text style={[styles.text, styles.textIconsRight]}>
+                        Minha lista
+                    </Text>
+                </View>
+                <View style={[styles.containIconRight]}>
+                    <Feather name="send" size={30} color="#fff" />
+                    <Text style={[styles.text, styles.textIconsRight]}>
+                        Compartilhe
+                    </Text>
+                </View>
+                <View style={[styles.containIconRight]}>
+                    <FontAwesome name="play" size={30} color="#fff" />
+                    <Text style={[styles.text, styles.textIconsRight]}>
+                        Assistir
+                    </Text>
+                </View>
+            </View>
+            <View style={[styles.containIconLeft]}>
+                <TouchableOpacity onPress={() => setMute(!mute)}>
+                    <Octicons
+                        name={mute ? 'unmute' : 'mute'}
+                        size={30}
+                        color="#fff"
+                    />
+                </TouchableOpacity>
+            </View>
+        </ImageBackground>
     </View>
 );
+
+const QuickLaugh: React.FC = () => {
+    const [mute, setMute] = useState<boolean>(false);
+
+    return (
+        <View style={[styles.contain]}>
+            <FlatList
+                data={videoQL}
+                keyExtractor={(i) => i.id.toString()}
+                renderItem={() => <VideoQL mute={mute} setMute={setMute} />}
+                snapToInterval={metrics.dimensions.height}
+                snapToAlignment="end"
+                decelerationRate="fast"
+            />
+        </View>
+    );
+};
 
 export default QuickLaugh;
--- src/Pages/private/QuickLaugh/styles.tsx
+++ src/Pages/private/QuickLaugh/styles.tsx
@@ -4,12 +4,60 @@ import { metrics } from '../../../services/metrics';
 const styles = StyleSheet.create({
     contain: {
         flex: 1,
-        backgroundColor: metrics.colors.color1,
+    },
+
+    containQL: {
+        flex: 1,
+        height: metrics.dimensions.height,
     },
 
     text: {
         fontFamily: metrics.fonts.defaultRegular,
-        color: metrics.colors.color4,
+        color: metrics.colors.color3,
+    },
+
+    containShort: {
+        flex: 1,
+    },
+
+    containAge: {
+        position: 'absolute',
+        right: 20,
+        top: 50,
+        backgroundColor: '#000000a6',
+        padding: 5,
+        borderRadius: 2,
+    },
+
+    containIconsRight: {
+        position: 'absolute',
+        right: 10,
+        bottom: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    containIconAvatar: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        marginBottom: 30,
+    },
+
+    textIconsRight: {
+        fontSize: 12,
+        marginTop: 10,
+    },
+
+    containIconRight: {
+        marginBottom: 20,
+        alignItems: 'center',
+    },
+
+    containIconLeft: {
+        position: 'absolute',
+        left: 20,
+        bottom: 40,
     },
 });
 
