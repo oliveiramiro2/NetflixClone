bd53165505eb37b67b11ec9ab81baf0f9e3b88bf
oliveiramiro2 <ramiromares95@gmail.com>
2023-02-02 21:30:25 -0300

    adjust on header

--- src/routes/private/index.tsx
+++ src/routes/private/index.tsx
@@ -169,6 +169,7 @@ const PrivateRoutes = () => (
                         />
                     </View>
                 ),
+                title: '',
                 headerTransparent: true,
                 headerStyle: { ...styles.headerContain },
             }}
